6a0029dae62440e6b767458a747e219230b1b4c5
Sraddha--aa <sraddha@mozilor.com>
2023-08-21 23:23:17 +0530

    sr

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,15 @@
 import Script from 'next/script';
+
 import Head from 'next/head'
 
 export default function Home() {
   return (
     <div>
       <Head>
-      <Script       
-  src={`https://cdn-cookieyes.com/client_data/7a5c4061e4800cd397a98eb3/script.js`}>
-  </Script>
-  strategy="beforeInteractive"
+        <Script       
+          src={`https://cdn-cookieyes.com/client_data/7a5c4061e4800cd397a98eb3/script.js`}>
+        </Script>
+        strategy="beforeInteractive"
         <title>Simple Next.js Project</title>
         <meta name="description" content="A simple Next.js project example" />
         <link rel="icon" href="/favicon.ico" />
